5208b35f7827c5ac7322bbd9edea5fd6acecafb1
Jianing Wu <jiwu6498@uni.sydney.edu.au>
2019-10-21 13:39:38 +1100

    add addPhraseToDisplay function

--- app.js
+++ app.js
@@ -9,18 +9,39 @@ let missed =0;
 
 //Attach a event listener to the “Start Game” button to hide the start screen overlay.
 const startButton =document.querySelector(".btn__reset");
+
 startButton.addEventListener("click",()=>{
    document.getElementById("overlay").style.display="none";
 });
 
 //Create a phrases array to contain at least 5 different phrases
-let phrases=["Hello there","This is Jenny","Welcome to my websites","Programming is fun","Enjoy everyday"];
+let phrases=["A Bird In The Hand Is Two In Bush","Amazon River","Welcome to my websites","Programming is fun","Happy everyday"];
 
 //Create a getRandomPhraseAsArray function.
 function getRandomPhraseAsArray(arr){
    let randomNumber=Math.floor(Math.random()*5);
-   let characterArray=arr[randomNumber].split(" ");
+   let characterArray=arr[randomNumber].split("");
    return characterArray;
 }
 
-console.log(getRandomPhraseAsArray(phrases));
+//Set the game display
+function addPhraseToDisplay(arr){
+   // do stuff any arr that is passed in, and add to `#phrase ul`
+   const ul=phrase.firstElementChild;
+
+   for(let i=0;i<arr.length;i++){
+      const li=document.createElement("li");
+      li.textContent=arr[i];
+      if(li.textContent!=" "){
+         li.className="letter";
+      }else{
+         li.className="space";
+      }
+      ul.appendChild(li);
+   }
+   phrase.appendChild(ul);
+}
+
+const phraseArray = getRandomPhraseAsArray(phrases);
+// console.log(phraseArray);
+addPhraseToDisplay(phraseArray); 
