7433e7b266b9fed5cd442017a16b30bfca486b37
Jianing Wu <jiwu6498@uni.sydney.edu.au>
2019-10-21 15:51:15 +1100

    add checkwin eventListener

--- app.js
+++ app.js
@@ -3,39 +3,39 @@ const qwerty = document.getElementById("qwerty");
 
 //Get the element with the ID of phrase and save it to a variable.
 const phrase = document.getElementById("phrase");
-const ul=phrase.firstElementChild;
+const ul = phrase.firstElementChild;
 
 //Create a missed variable to keep the track of the number of guesses
-let missed =0;
+let missed = 0;
 
 //Attach a event listener to the “Start Game” button to hide the start screen overlay.
-const startButton =document.querySelector(".btn__reset");
+const startButton = document.querySelector(".btn__reset");
 
-startButton.addEventListener("click",()=>{
-   document.getElementById("overlay").style.display="none";
+startButton.addEventListener("click", () => {
+   document.getElementById("overlay").style.display = "none";
 });
 
 //Create a phrases array to contain at least 5 different phrases
-let phrases=["A Bird In The Hand Is Two In Bush","Amazon River","Welcome to my websites","Programming is fun","Happy everyday"];
+let phrases = ["Good Morning", "Amazon River", "Welcome", "Programming is fun", "Happy everyday"];
 
 //Create a getRandomPhraseAsArray function.
-function getRandomPhraseAsArray(arr){
-   let randomNumber=Math.floor(Math.random()*5);
-   let characterArray=arr[randomNumber].toLowerCase().split("");
+function getRandomPhraseAsArray(arr) {
+   let randomNumber = Math.floor(Math.random() * 5);
+   let characterArray = arr[randomNumber].toLowerCase().split("");
    return characterArray;
 }
 
 //Set the game display
-function addPhraseToDisplay(arr){
+function addPhraseToDisplay(arr) {
    // do stuff any arr that is passed in, and add to `#phrase ul`
-  
-   for(let i=0;i<arr.length;i++){
-      const li=document.createElement("li");
-      li.textContent=arr[i];
-      if(li.textContent!=" "){
-         li.className="letter";
-      }else{
-         li.className="space";
+
+   for (let i = 0; i < arr.length; i++) {
+      const li = document.createElement("li");
+      li.textContent = arr[i];
+      if (li.textContent != " ") {
+         li.className = "letter";
+      } else {
+         li.className = "space";
       }
       ul.appendChild(li);
    }
@@ -43,25 +43,56 @@ function addPhraseToDisplay(arr){
 }
 
 const phraseArray = getRandomPhraseAsArray(phrases);
-// console.log(phraseArray);
-addPhraseToDisplay(phraseArray); 
-
+console.log(phraseArray);
+addPhraseToDisplay(phraseArray);
+const letters = document.getElementsByClassName("letter");
 //Create a checkLetter function.
-function checkLetter(keyboardClicked){
-   const letters=document.getElementsByClassName("letter");
-   for(let i=0;i<letters.length;i++){
-      if(letters[i].textContent===keyboardClicked){
-         letters[i].className+=" show";
-         // console.log(letters[i]);
-      }else{
-         return null;
+function checkLetter(button) {
+   let letter = null;
+   for (let i = 0; i < letters.length; i++) {
+      if (letters[i].textContent === button.textContent) {
+         letters[i].className += " show";
+         letter = letters[i].textContent;
       }
    }
+   if (letter != null) {
+      return letter;
+   } else {
+      return null;
+   }
 };
+const shows=document.getElementsByClassName("show");
+//Count the missed guesses in the game
+const scoreboard = document.getElementById("scoreboard");
+//Add an event listener to the keyboard.
+qwerty.addEventListener("click", (event) => {
+   if (event.target.tagName === "BUTTON") {
+      const button = event.target;
+      const div = event.target.parentNode;
+      button.className = "chosen";
+      button.setAttribute("disabled", true);
+      const letterFound = checkLetter(button);
+      if (letterFound === null) {
+         missed = missed + 1;
+         const ol = scoreboard.firstElementChild;
+         const li = ol.lastElementChild;
+         ol.removeChild(li);
+      }
+      checkWin();
+   }
+   
+   function checkWin() {
+      console.log(shows.length);
+      console.log(letters.length);
+      if(shows.length===letters.length){
+         document.getElementById("overlay").className="win";
+         document.getElementById("overlay").style.display="block";
+      }
 
-// checkLetter("a");
+      if(missed>=5){
+         document.getElementById("overlay").className="lose";
+         document.getElementById("overlay").style.display="block";
+      }
 
-//
-qwerty.addEventListener("click",(event)=>{
-
+   }
 });
